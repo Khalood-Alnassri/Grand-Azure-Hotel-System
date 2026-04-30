8488c4eb5ddf81572255f8a604731e6a987ebb7a
Khalood.kk <alnasseikallood@gmail.com>
2026-04-30 10:02:22 +0400

    add guest methods in class guest and class hotil

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace Grand_Azure_Hotel_System
 {
+
     internal class Program
     {
+        public static Hotel hotil = new Hotel("Grand Azure Hotel");
+
         public static void DisplayMenu()
         {
+            Console.WriteLine("====================================");
             Console.WriteLine("===== Grand Azure Hotel System =====");
             Console.WriteLine("1. Add Guest.");
             Console.WriteLine("2. Add Room.");
@@ -16,6 +21,7 @@ namespace Grand_Azure_Hotel_System
             Console.WriteLine("7. Search Guest by National ID.");
             Console.WriteLine("8. Show Hotel Statistics.");
             Console.WriteLine("9. Exit");
+            Console.WriteLine("====================================");
         }
 
         public static int ChooseOption()
@@ -38,10 +44,20 @@ namespace Grand_Azure_Hotel_System
             }
         }
 
-
-
-        static void Main(string[] args)
+        // case 1: function to add new guest
+        public static void AddGuest()
         {
+            Console.WriteLine("Enter the name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter national ID: ");
+            string id = Console.ReadLine();
+
+            hotil.AddGuest(name, id);
+        }
+
+     
+        static void Main(string[] args)
+        { 
             bool exit = false;
             while (!exit)
             {
@@ -51,7 +67,9 @@ namespace Grand_Azure_Hotel_System
                  switch (option)
                  { 
                     case 1:
-                      
+
+                        AddGuest();
+
                         break;
 
                     case 2:
@@ -97,18 +115,68 @@ namespace Grand_Azure_Hotel_System
 
     class Guest
     {
-        private static string fullName;
-        private static string nationalID;
+        private string fullName;
+        private string nationalID;
 
         private static int totalGuestsCreated = 0;
+
+        // Property to set name
+        public void SetName(string guest)
+        {
+            if (guest.IsWhiteSpace())
+            {
+                Console.WriteLine("Please enter a valid name!");
+            }
+
+            else
+            {
+                fullName = guest;
+            }
+        }
+
+        // Property to get name
+        public string GetName()
+        {
+            return fullName;
+        }
+
+        // Property to get-only national ID
+        public string GetNationalID()
+        {
+            return nationalID;
+        }
+
+        // method to count total guest created
+        public static int getTotalGuestsCreated()
+        {
+            return totalGuestsCreated; 
+        }
+
+        // const to add new guest
+        public Guest(string name, string ID)
+        {
+            this.fullName = name;
+            this.nationalID = ID;
+            totalGuestsCreated++;
+        }
+
+        // method to display guest information
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Guest name: " + fullName + " | " + "National ID: " + nationalID);
+        }
+
 
     }
 
     class Room
     {
-        static int roomNumber;
-        static string roomType;
-        static bool isBooked;
+        int roomNumber;
+        string roomType;
+        bool isBooked;
+
+    
+
 
 
     }
@@ -128,6 +196,40 @@ namespace Grand_Azure_Hotel_System
         private List<Guest> guests = new List<Guest>();
         private List<Room> rooms = new List<Room>();
         private List<Booking> bookings = new List<Booking>();
+
         public static string HotelName;
+
+        public Hotel(string name)
+        {
+            HotelName = name;
+            guests = new List<Guest>();
+            rooms = new List<Room>();
+            bookings = new List<Booking>();
+        }
+
+        //Guest Methods//
+        // add new guest
+        public void AddGuest(string name, string id)
+        {
+            foreach (Guest guest in guests)
+            {
+                if(guest.GetNationalID() == id)
+                {
+                    Console.WriteLine("The guest is already exist.");
+                    return;
+                }
+            }
+
+            guests.Add(new Guest(name, id));
+        }
+
+        // find guest by national id
+        public Guest FindGuest(string id)
+        {
+            return guests.Find(g => g.GetNationalID() == id);
+        }
+
+
+
     }
 }
